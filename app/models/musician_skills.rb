ce4cb1c05af6c0a59e484e499bfe25751510822a
YanPam <https://github.com/YanPam>
2019-08-06 16:49:02 +0300

    fix v3

--- app/models/musician_skills.rb
+++ app/models/musician_skills.rb
@@ -1,4 +1,6 @@
+# rubocop:disable Lint/Syntax
 class MusicianSkill < ApplicationRecord
   has_many :musicianskilsusers
   has_many :user through :musicianskillsusers
 end
+# rubocop:enable Lint/Syntax
