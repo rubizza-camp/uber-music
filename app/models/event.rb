db3fb1657d9550791aec1cc3b92be952c96893f5
Maxxx911 <maxrikal@gmail.com>
2019-08-08 19:39:38 +0300

    Edit organization, add enum to edit model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,27 @@
 class Event < ApplicationRecord
   belongs_to :place
   has_many :images, as: :imageable
-  has_many :organization_event
-  has_many :organizations, through: :organization_event
+  has_many :organization_events
+  has_many :organizations, through: :organization_events
+  has_many :pending_organization_events,
+           -> { where status: OrganizationEvent.statuses[:pending] },
+           class_name: 'OrganizationEvent'
+  has_many :pending_events,
+           through: :pending_organization_events,
+           class_name: 'Event',
+           source: :event
+  has_many :disabled_organization_events,
+           -> { where status: OrganizationEvent.statuses[:disabled] },
+           class_name: 'OrganizationEvent'
+  has_many :disabled_events,
+           through: :disabled_organization_events,
+           class_name: 'Event',
+           source: :event
+  has_many :approved_organization_events,
+           -> { where status: OrganizationEvent.statuses[:approved] },
+           class_name: 'OrganizationEvent'
+  has_many :approved_events,
+           through: :approved_organization_events,
+           class_name: 'Event',
+           source: :event
 end
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,16 +3,25 @@ class Organization < ApplicationRecord
   has_many :users, through: :user_organizations
   has_many :organization_events
   has_many :events, through: :organization_events
-  has_many :pending_organization_events, -> { where status: OrganizationEvent.status[:pending] },
+  has_many :pending_organization_events,
+           -> { where status: OrganizationEvent.statuses[:pending] },
            class_name: 'OrganizationEvent'
-  has_many :pending_events, through: :pending_organization_events, class_name: 'Event',
-                            source: :event
-  has_many :disabled_organization_events, -> { where status: OrganizationEvent.status[:disabled] },
+  has_many :pending_events,
+           through: :pending_organization_events,
+           class_name: 'Event',
+           source: :event
+  has_many :disabled_organization_events,
+           -> { where status: OrganizationEvent.statuses[:disabled] },
            class_name: 'OrganizationEvent'
-  has_many :disabled_events, through: :disabled_organization_events, class_name: 'Event',
-                             source: :event
-  has_many :approved_organization_events, -> { where status: OrganizationEvent.status[:approved] },
+  has_many :disabled_events,
+           through: :disabled_organization_events,
+           class_name: 'Event',
+           source: :event
+  has_many :approved_organization_events,
+           -> { where status: OrganizationEvent.statuses[:approved] },
            class_name: 'OrganizationEvent'
-  has_many :approved_events, through: :approved_organization_events, class_name: 'Event',
-                             source: :event
+  has_many :approved_events,
+           through: :approved_organization_events,
+           class_name: 'Event',
+           source: :event
 end
