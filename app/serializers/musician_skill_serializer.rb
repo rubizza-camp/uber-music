f5b888f292283277f1c25f1407c15697b105f0d4
YanPam <https://github.com/YanPam>
2019-08-09 14:42:54 +0300

    Seed Part 1

--- app/serializers/musician_skill_serializer.rb
+++ app/serializers/musician_skill_serializer.rb
@@ -1,5 +1,4 @@
 class MusicianSkillSerializer < ActiveModel::Serializer
-<<<<<<< HEAD
   attributes :id, :name
   has_many :musician_skill_users
   has_many :users, through: :musician_skill_users
@@ -12,21 +11,4 @@ class MusicianSkillSerializer < ActiveModel::Serializer
   has_many :approved_users,
            through: :approved_musician_skill_users,
            source: :user
-=======
-  attributes :name
-  has_many :musician_skill_users
-  has_many :users, through: :musician_skill_users
-  has_many :pending_musician_skills,
-           through: :pending_musician_skill_users,
-           class_name: 'MusicianSkill',
-           source: :musician_skill
-  has_many :disabled_musician_skills,
-           through: :disabled_musician_skill_users,
-           class_name: 'MusicianSkill',
-           source: :musician_skill
-  has_many :approved_musician_skills,
-           through: :approved_musician_skill_users,
-           class_name: 'MusicianSkill',
-           source: :musician_skill
->>>>>>> Work correct and fixed functions
 end
