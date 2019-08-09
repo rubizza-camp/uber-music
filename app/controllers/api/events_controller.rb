ca9dc0b952c93b5f1780c21a22aeaedb09fa3d17
Maxxx911 <maxrikal@gmail.com>
2019-08-09 11:40:27 +0300

    Fixed reek!

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,40 +1,43 @@
-class Api::EventsController < ApplicationController
-  def index
-    @events = Event.all
-    render json: @events
-  end
-
-  def show
-    @event = Event.find(params[:id])
-    render json: @event
-  end
-
-  def create
-    @event = Event.new(events_params)
-    if @event.save
-      render json: @event, status: :created
-    else
-      render json: @event.errors, status: :unprocessable_entity
+module Api
+  class EventsController < ApplicationController
+    def index
+      @events = Event.all
+      render json: @events
     end
-  end
 
-  def update
-    @event = Event.find(params[:id])
-    if @event.update(events_params)
-      render json: @event, status: :ok
-    else
-      render json: @event.errors, status: :unprocessable_entity
+    def show
+      @event = Event.find(params[:id])
+      render json: @event
     end
-  end
 
-  def destroy
-    @event = Event.find(params[:id])
-    @event.destroy
-    head :no_content
-  end
-  
-  private
+    def create
+      @event = Event.new(events_params)
+      if @event.save
+        render json: @event, status: :created
+      else
+        render json: @event.errors, status: :unprocessable_entity
+      end
+    end
+
+    def update
+      @event = Event.find(params[:id])
+      if @event.update(events_params)
+        render json: @event, status: :ok
+      else
+        render json: @event.errors, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @event = Event.find(params[:id])
+      @event.destroy
+      head :no_content
+    end
+
+    private
+
     def events_params
       params.require(:events).permit(:id, :description, :name, :start_time, :end_time)
     end
+  end
 end
