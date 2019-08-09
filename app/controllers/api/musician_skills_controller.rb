cf37df71f213fb42bc6e04889f410b9eaf09c96c
YanPam <https://github.com/YanPam>
2019-08-09 12:27:22 +0300

    Fix hound and rename some files

--- app/controllers/api/musician_skills_controller.rb
+++ app/controllers/api/musician_skills_controller.rb
@@ -1,31 +1,31 @@
 module Api
   class MusicianSkillsController < ApplicationController
     def index
-	    @musican_skill = MusicianSkill.all
-  	  render json: @musican_skill
+      @musican_skill = MusicianSkill.all
+      render json: @musican_skill
     end
 
     def show
-	    @musican_skill = MusicianSkill.find(params[:id])
-	    render json: @musican_skill
+      @musican_skill = MusicianSkill.find(params[:id])
+      render json: @musican_skill
     end
 
     def create
-  	  @musican_skill = MusicianSkill.new(skill_params)
-    	if @musican_skill.save
-    	  render json: @musican_skill, status: :created
-    	else
-	      render json: @musican_skill.errors, status: :unprocessable_entity 
-    	end 
+      @musican_skill = MusicianSkill.new(skill_params)
+      if @musican_skill.save
+        render json: @musican_skill, status: :created
+      else
+        render json: @musican_skill.errors, status: :unprocessable_entity
+      end
     end
 
     def update
-    	@musican_skill = MusicianSkill.find(params[:id])
+      @musican_skill = MusicianSkill.find(params[:id])
       if @musican_skill.update(skill_params)
         render json: @musican_skill, status: :ok
       else
         render json: @musican_skill.errors, status: :unprocessable_entity
-      end	
+      end
     end
 
     def destroy
@@ -37,7 +37,7 @@ module Api
     private
 
     def skill_params
-	    params.require(:musician_skill).permit(:id, :name)
+      params.require(:musician_skill).permit(:id, :name)
     end
   end
 end
