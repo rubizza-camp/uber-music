2daa1614a4ee07df62154b41e6c427145499bead
YanPam <https://github.com/YanPam>
2019-08-09 17:39:54 +0300

    Seed Part2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,1 @@
-<<<<<<< HEAD
 User.create(nickname: 'eqe', first_name: 'qwe', second_name: 'quno', type: 'User', email: 'rynf', password: 'tntfm')
-=======
-User.create(nickname: 'Nagibator2006', first_name: 'Alesha', second_name: 'Pupkin', type: 'User', email: 'lolo@mail.ru', password: 'qwerty')
-User.create(nickname: 'MomLover', first_name: 'Peta', second_name: 'Fufel', type: 'User', email: 'agaaa@mail.ru', password: '12345')
-User.create(nickname: '__xxx_Killer_xxx__', first_name: 'Kola', second_name: 'Ivanv', type: 'User', email: 'reeo@mail.ru', password: 'qwerty123456')
-
-Organization.create(name: 'United Russia', description: 'True lover of motherland')
-
-MusicianSkill.create(name: 'Vocals')
-
-Genre.create(name: 'neo-psychedelia', description: 'Neo-psychedelia is a diverse genre of psychedelic music that originated in the 1970s as an outgrowth of the British post-punk scene, also called acid punk.')
-
-Place.create(name: 'Hell', latitude: '666', longitude: '666', address: 'Kazintsa street 60, Minsk', description: 'Muaxaxaaxxa', rules: 'no socks with sandals')
-
-Event.create(place: 'Hell', name: 'New Year', description: 'Happy ')
->>>>>>> Seed Part 1
