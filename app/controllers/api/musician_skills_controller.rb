f6f9dbe750c3b405aab123ad7a334b868bec7c59
YanPam <https://github.com/YanPam>
2019-08-09 11:55:31 +0300

    Create controleer for musican scills model

---
+++ app/controllers/api/musician_skills_controller.rb
@@ -0,0 +1,43 @@
+module Api
+  class MusicianSkillsController < ApplicationController
+    def index
+	    @musican_skill = MusicianSkill.all
+  	  render json: @musican_skill
+    end
+
+    def show
+	    @musican_skill = MusicianSkill.find(params[:id])
+	    render json: @musican_skill
+    end
+
+    def create
+  	  @musican_skill = MusicianSkill.new(skill_params)
+    	if @musican_skill.save
+    	  render json: @musican_skill, status: :created
+    	else
+	      render json: @musican_skill.errors, status: :unprocessable_entity 
+    	end 
+    end
+
+    def update
+    	@musican_skill = MusicianSkill.find(params[:id])
+      if @musican_skill.update(skill_params)
+        render json: @musican_skill, status: :ok
+      else
+        render json: @musican_skill.errors, status: :unprocessable_entity
+      end	
+    end
+
+    def destroy
+      @musican_skill = MusicianSkill.find(params[:id])
+      @musican_skill.delete
+      head :no_content
+    end
+
+    private
+
+    def skill_params
+	    params.require(:musician_skill).permit(:id, :name)
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_08_09_111639) do
-
-  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "events", force: :cascade do |t|
@@ -34,8 +32,8 @@ ActiveRecord::Schema.define(version: 2019_08_09_111639) do
   end
 
   create_table "images", force: :cascade do |t|
-    t.integer "imageable_id", null: false
-    t.string "imageable_type", null: false
+    t.bigint "imageable_id"
+    t.string "imageable_type"
     t.string "url", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -48,6 +46,7 @@ ActiveRecord::Schema.define(version: 2019_08_09_111639) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "status", default: 0, null: false
+
     t.index ["musician_skill_id"], name: "index_musician_skill_users_on_musician_skill_id"
     t.index ["user_id"], name: "index_musician_skill_users_on_user_id"
   end
