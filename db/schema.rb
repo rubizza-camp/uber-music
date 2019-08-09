4c63aa56a4749880ada4f4ea3f85259777006e65
Maxxx911 <maxrikal@gmail.com>
2019-08-09 14:21:22 +0300

    Add column status to musician_skill_users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_08_171446) do
+ActiveRecord::Schema.define(version: 2019_08_09_111639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2019_08_08_171446) do
   create_table "musician_skill_users", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "musician_skills_id"
+    t.integer "status", default: 0
     t.index ["musician_skills_id"], name: "index_musician_skill_users_on_musician_skills_id"
     t.index ["user_id"], name: "index_musician_skill_users_on_user_id"
   end
