244a113cb0923186e710c90cd9a269b98a5fe070
Maxxx911 <maxrikal@gmail.com>
2019-08-12 11:10:11 +0300

    fix pending organizations for serializer

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,18 +1,17 @@
 class EventSerializer < ActiveModel::Serializer
   attributes :id, :description, :name, :start_time, :end_time
   belongs_to :place
-  has_many :organizations, through: :organization_events
-  has_many :pending_events,
+  has_many :pending_organizations,
            through: :pending_organization_events,
-           class_name: 'Event',
-           source: :event
-  has_many :disabled_events,
+           class_name: 'Organization',
+           source: :organization
+  has_many :disabled_organizations,
            through: :disabled_organization_events,
-           class_name: 'Event',
-           source: :event
-  has_many :approved_events,
+           class_name: 'Organization',
+           source: :organization
+  has_many :approved_organizations,
            through: :approved_organization_events,
-           class_name: 'Event',
-           source: :event
+           class_name: 'Organization',
+           source: :organization
   has_many :images, as: :imageable
 end
