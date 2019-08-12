31c90812a82b99470ea8b67a25dcd64a599b7946
YanPam <https://github.com/YanPam>
2019-08-12 11:01:58 +0300

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,39 +1,39 @@
-User.create(nickname: 'Petya_Super', first_name: 'Petya', second_name: 'Ivanov', type: 'User', email: 'petya2006@mail.ru', password: '123123')
+user =User.create(nickname: 'Petya_Super', first_name: 'Petya', second_name: 'Ivanov', type: 'User', email: 'petya2006@mail.ru', password: '123123')
 
 Genre.create(name: 'Metall', description: 'Some rock style')
 
-User.all.first.genres << Genre.all.first
+user.genres << Genre.first
 
-Organization.create(name: 'Moon wolfs', description: 'Hard rock organization')
+organization = Organization.create(name: 'Moon wolfs', description: 'Hard rock organization')
 
-User.all.first.organizations << Organization.all.first
+user.organizations << Organization.all.first
 
-Place.create(name: 'Kamenka', latitude: '1488', longitude: '228', address: 'Pushkina street', description: 'Happy place', rules: 'No latinas')
+kamenka = Place.create(name: 'Kamenka', latitude: '1488', longitude: '228', address: 'Pushkina street', description: 'Happy place', rules: 'No latinas')
 
-Place.create(name: 'Urucha', latitude: '8800', longitude: '555', address: 'Tolstova street', description: 'Not happy place', rules: 'No latinas and afro')
+urucha = Place.create(name: 'Urucha', latitude: '8800', longitude: '555', address: 'Tolstova street', description: 'Not happy place', rules: 'No latinas and afro')
 
-Place.create(name: 'Nemiga', latitude: '3535', longitude: '328', address: 'Mishkina street', description: 'Horable place', rules: 'No afro')
+nemiga = Place.create(name: 'Nemiga', latitude: '3535', longitude: '328', address: 'Mishkina street', description: 'Horable place', rules: 'No afro')
 
-Event.create(place_id: "#{Place.all.first.id}", name: 'New Year', description: 'Happy Event', start_time: 'Sun, 03 Mar 2019 14:17:01 +0000', end_time: 'Sun, 03 Mar 2019 14:17:01 +0000')
+new_year = Event.create(place_id: "#{kamenka.id}", name: 'New Year', description: 'Happy Event', start_time: 'Sun, 03 Mar 2019 14:17:01 +0000', end_time: 'Sun, 03 Mar 2019 14:17:01 +0000')
 
-Event.create(place_id: "#{Place.all.second.id}", name: 'Hallowin', description: 'Event with goast', start_time: 'Sun, 20 Mar 2019 14:17:01 +0000', end_time: 'Sun, 30 Mar 2019 14:17:01 +0000')
+hallowin = Event.create(place_id: "#{urucha.id}", name: 'Hallowin', description: 'Event with goast', start_time: 'Sun, 20 Mar 2019 14:17:01 +0000', end_time: 'Sun, 30 Mar 2019 14:17:01 +0000')
 
-Event.create(place_id: "#{Place.all.last.id}", name: 'Birthday', description: 'Event with cake', start_time: 'Sun, 10 Mar 2019 14:17:01 +0000', end_time: 'Sun, 11 Mar 2019 14:17:01 +0000')
+birthday = Event.create(place_id: "#{nemiga.id}", name: 'Birthday', description: 'Event with cake', start_time: 'Sun, 10 Mar 2019 14:17:01 +0000', end_time: 'Sun, 11 Mar 2019 14:17:01 +0000')
 
-Organization.all.first.pending_events << Event.all.first
+organization.pending_events << new_year
 
-Organization.all.first.disabled_events << Event.all.second
+organization.disabled_events << hallowin
 
-Organization.all.first.approved_events << Event.all.last
+organization.approved_events << birthday
 
-MusicianSkill.create(name: 'Bass')
+bass_skill = MusicianSkill.create(name: 'Bass')
 
-MusicianSkill.create(name: 'Dram')
+drum_skill = MusicianSkill.create(name: 'Drum')
 
-MusicianSkill.create(name: 'Fleute')
+fleute_skill = MusicianSkill.create(name: 'Fleute')
 
-User.all.first.pending_musician_skills << MusicianSkill.all.first
+user.pending_musician_skills << bass_skill
 
-User.all.first.disabled_musician_skills << MusicianSkill.all.second
+user.disabled_musician_skills << drum_skill
 
-User.all.first.approved_musician_skills << MusicianSkill.all.last
+user.approved_musician_skills << fleute_skill
--- spec/controllers/musician_skills_controller_spec.rb
+++ spec/controllers/musician_skills_controller_spec.rb
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-=======
-require 'rails_helper'
-
->>>>>>> Create controleer for musican scills model
 RSpec.describe MusicianSkillsController, type: :controller do
   describe "GET #index" do
     it "returns http success" do
