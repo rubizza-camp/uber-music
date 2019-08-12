c5f94a4949f7a5b32bfa99370c6d7566b4fb1885
YanPam <https://github.com/YanPam>
2019-08-12 10:41:46 +0300

    Remove class_name

--- app/models/musician_skill.rb
+++ app/models/musician_skill.rb
@@ -6,20 +6,17 @@ class MusicianSkill < ApplicationRecord
            class_name: 'MusicianSkillUser'
   has_many :pending_users,
            through: :pending_musician_skill_users,
-           class_name: 'User',
            source: :user
   has_many :disabled_musician_skill_users,
            -> { where status: MusicianSkillUser.statuses[:disabled] },
            class_name: 'MusicianSkillUser'
   has_many :disabled_users,
            through: :disabled_musician_skill_users,
-           class_name: 'User',
            source: :user
   has_many :approved_musician_skill_users,
            -> { where status: MusicianSkillUser.statuses[:approved] },
            class_name: 'MusicianSkillUser'
   has_many :approved_users,
            through: :approved_musician_skill_users,
-           class_name: 'MusicianSkill',
            source: :musician_skill
 end
