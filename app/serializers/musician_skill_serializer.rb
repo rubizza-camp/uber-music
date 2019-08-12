2897f71b08310c741e0ee001417a85258a9f2b9b
YanPam <https://github.com/YanPam>
2019-08-12 10:14:16 +0300

    modified musician skill

--- app/serializers/musician_skill_serializer.rb
+++ app/serializers/musician_skill_serializer.rb
@@ -1,16 +1,16 @@
 class MusicianSkillSerializer < ActiveModel::Serializer
-  attributes :name
+  attributes :id, :name
   has_many :musician_skill_users
   has_many :users, through: :musician_skill_users
-  has_many :pending_musician_skills,
+  has_many :pending_users,
            through: :pending_musician_skill_users,
            class_name: 'MusicianSkill',
            source: :musician_skill
-  has_many :disabled_musician_skills,
+  has_many :disabled_users,
            through: :disabled_musician_skill_users,
            class_name: 'MusicianSkill',
            source: :musician_skill
-  has_many :approved_musician_skills,
+  has_many :approved_users,
            through: :approved_musician_skill_users,
            class_name: 'MusicianSkill',
            source: :musician_skill
