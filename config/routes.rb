796faf9ef34fb246f8a8c233e99d5be67f56bb4d
Maxxx911 <maxrikal@gmail.com>
2019-08-13 16:02:42 +0300

    Add route for activeadmin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :genres
   resources :events
   resources :places
