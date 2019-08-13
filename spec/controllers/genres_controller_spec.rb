932f9c0de7468b18d0ff290e778c6c12c0e5b5f2
Alesia Yakutsionak <als6000@gmail.com>
2019-08-13 15:06:51 +0300

    DEV031 Change API architecture

--- spec/controllers/genres_controller_spec.rb
+++ spec/controllers/genres_controller_spec.rb
@@ -1,6 +1,4 @@
-require 'rails_helper'
-
-RSpec.describe Api::GenresController, type: :controller do
+RSpec.describe GenresController, type: :controller do
   describe "GET #index" do
     it "returns http success" do
       get :index
--- spec/controllers/places_controller_spec.rb
+++ spec/controllers/places_controller_spec.rb
@@ -1,6 +1,4 @@
-require 'rails_helper'
-
-RSpec.describe Api::PlacesController do
+RSpec.describe PlacesController do
 
   describe "GET #index" do
 
