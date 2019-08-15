7127896103d015524ba2df1331a5180e56b64744
Alesia Yakutsionak <als6000@gmail.com>
2019-08-15 09:44:51 +0300

    Add styles for index and show pages for place model

--- app/javascript/components/PlacesIndex.jsx
+++ app/javascript/components/PlacesIndex.jsx
@@ -1,7 +1,33 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+    card: {
+      minWidth: 275,
+    },
+    bullet: {
+      display: 'inline-block',
+      margin: '0 2px',
+      transform: 'scale(0.8)',
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+  });
 
 class PlacesIndex extends React.Component {
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+
   constructor(props) {
     super(props);
     this.state = {
@@ -15,19 +41,25 @@ class PlacesIndex extends React.Component {
 
   render () {
     return(
-      <div>
+      <Card className={classes.card}>
         { this.state.places.map((place) =>
-          <div>
-            <a href={this.make_url(place.id)}>Name: {place.name}</a>
-            <h5>Latitude: {place.latitude}</h5>
-            <h5>Longitude: {place.longitude}</h5>
-            <h5>Address: {place.address}</h5>
-            <h4>Description: {place.description}</h4>
-            <h4>Rules: {place.rules}</h4>
-            <h4>\\\\\\\\\\\\\\</h4>
-          </div>
+          <CardContent>
+            <Typography variant="h5" component="h2">
+              {bull}
+              {place.name}
+            </Typography>
+            <Typography className={classes.title} color="textSecondary" gutterBottom>
+              {place.latitude}, {place.longitude}
+            </Typography>
+            <Typography variant="body2" component="p">
+              Address: {place.address}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small" to={this.make_url(place.id)}>Learn More</Button>
+          </CardActions>
         )}
-      </div>
+      </Card>
     );
   }
 }
