5f61e12ec753c553df3f2838cda816da3c7e75cb
AS-AlStar <starovojtov.alexander@gmail.com>
2019-08-15 19:17:29 +0300

    Split method inizialize

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-# rubocop:disable Metrics/MethodLength, Metrics/AbcSize
-
 class Ability
   include CanCan::Ability
   def initialize(user)
@@ -9,22 +7,28 @@ class Ability
       user = User.new
       user.update(type: 'Guest')
     end
+    autorizing(user)
+  end
 
+  def autorizing(user)
     if user.role?('Moderator')
       can :manage, :all
     elsif user.role?('User')
-      can %i[index show], [Event, Genre, Organization, MusicianSkill, User]
-      can :manage, Event do |event|
-        user.organization_ids.each do |organization_id|
-          event.organization_ids.include?(organization_id)
-        end
-      end
-      can :manage, Organization do |organization|
-        organization if organization.user_ids.include?(user.id)
-      end
+      user_rules
     else
       can %i[index show], [Place, Event]
     end
   end
+
+  def user_rules
+    can %i[index show], [Event, Genre, Organization, Place, MusicianSkill, User]
+    can :manage, Event do |event|
+      user.organization_ids.each do |organization_id|
+        event.organization_ids.include?(organization_id)
+      end
+    end
+    can :manage, Organization do |organization|
+      organization if organization.user_ids.include?(user.id)
+    end
+  end
 end
-# rubocop:enable Metrics/MethodLength, Metrics/AbcSize
