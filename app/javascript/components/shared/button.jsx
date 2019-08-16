c90c9de405575cab16099c81055383ab974aa7ab
Alesia Yakutsionak <als6000@gmail.com>
2019-08-16 14:35:00 +0300

    Add card, button and image components

---
+++ app/javascript/components/shared/button.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+  purpleColor: {
+    backgroundColor: '#9c27b0',
+      '&:hover': {
+        backgroundColor:'#c023db',
+      },
+  }
+}));
+
+
+export default function MediumButton(props) {
+  const classes = useStyles();
+    return (
+      <Button variant="contained" size="medium" color='primary' className={classes.purpleColor}>
+        {props.content}
+      </Button>
+    );
+}
+
---
+++ app/javascript/components/shared/image.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import { width } from '@material-ui/system';
+
+export default function MyImage(props) {
+    const useStyles = makeStyles(theme => ({
+        imgRaised: {
+            boxShadow:
+                "0 5px 15px -8px rgba(0, 0, 0, 0.24), 0 8px 10px -5px rgba(0, 0, 0, 0.2)"
+        },
+        imgRounded: {
+            borderRadius: "6px !important"
+        },
+        imgFluid: {
+            width: props.width,
+            height: props.height
+        }
+    }));
+
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+            <img
+                src={props.src}
+                alt="..."
+                className={
+                    classes.imgRaised +
+                    " " +
+                    classes.imgRounded +
+                    " " +
+                    classes.imgFluid
+                }
+            />
+        </div>
+    );
+}
