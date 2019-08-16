2587ad963ab4cd109c6123c5fdcdb820f0a4a986
AS-AlStar <starovojtov.alexander@gmail.com>
2019-08-16 10:31:01 +0300

    Create title component

---
+++ app/javascript/components/shared/title.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import TypoGraphy from '@material-ui/core/Typography'
+import Box from '@material-ui/core/Box'
+
+
+export default function Title(props) {
+    return (
+        <TypoGraphy  component="h3">
+            <Box fontSize="1.9em" fontWeight={350}>
+                {props.content}
+            </Box>
+        </TypoGraphy>
+    );
+}
