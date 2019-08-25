681583c2dddeacf8e756b7f9412e4166ae462061
MariaPaharelava <paharelava.maria@gmail.com>
2019-08-25 13:40:25 +0300

    DEV048 fix frame for react components

--- app/javascript/components/organizations/organization_title.jsx
+++ app/javascript/components/organizations/organization_title.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import Grid from '@material-ui/core/Grid';
-import TypoGraphy from '@material-ui/core/Typography'
-import Box from '@material-ui/core/Box'
 import MediumButton from '../shared/button'
 import OrganizationTitleName from './organization_title_name'
 
--- app/javascript/components/users/user_index.jsx
+++ app/javascript/components/users/user_index.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-import classNames from "classnames";
 import withStyles from "@material-ui/core/styles/withStyles";
 import landingPageStyle from "../shared/landingPage";
 import Grid from "@material-ui/core/Grid";
@@ -8,22 +7,16 @@ import UserCard from "./user_card.jsx";
 
 class UserIndexLandingPage extends React.Component {
   render() {
-    const {classes, users} = this.props;
+    const {users} = this.props;
     return (
       <div>
-        <div>
-          <div className={classNames(classes.main, classes.mainRaised)}>
-            <div className={classes.container}>
-              <Grid container direction="row" justify="center" alignItems="stretch" spacing={1}>
-                {users.map((user, i) =>
-                  <Grid item >
-                    <UserCard key={i} user={user} link={'/users/' + user.id }/>
-                  </Grid>
-                )}
-              </Grid>
-            </div>
-          </div>
-        </div>
+        <Grid container direction="row" justify="center" alignItems="stretch" spacing={1}>
+          {users.map((user, i) =>
+            <Grid item>
+              <UserCard key={i} user={user} link={'/users/' + user.id}/>
+            </Grid>
+          )}
+        </Grid>
       </div>
     );
   }
