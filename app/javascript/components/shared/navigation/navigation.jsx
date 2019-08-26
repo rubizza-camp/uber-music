c17de26ca6b89ba7110106c7fcb6f7a1bf81cff5
Alesia Yakutsionak <als6000@gmail.com>
2019-08-26 13:26:31 +0300

    Add create event button

--- app/javascript/components/shared/navigation/navigation.jsx
+++ app/javascript/components/shared/navigation/navigation.jsx
@@ -6,153 +6,151 @@ import CustomDropdown from "./dropdown";
 import withStyles from "@material-ui/core/styles/withStyles";
 import Button from "./navigation_button";
 import Header from "./header";
+import DialogWindow from "./dialog_window";
 
 import navbarsStyle from "../style/navbarsStyle";
 
-class Navigation extends React.Component {
-  render() {
-    const {classes} = this.props;
-    if (this.props.id) {
-      return (
-        <Header
-          brand="Пешеходка"
-          color="dark"
-          fixed
-          rightLinks={
-            <List className={classes.list}>
-              <ListItem className={classes.listItem}>
-                <Button
-                  href="/organizations"
-                  className={classes.navLink}
-                  color="transparent"
-                  >
-                  Organizations
-                </Button>
-            </ListItem>
-              <ListItem className={classes.listItem}>
-                <Button
-                  href="/places"
-                  className={classes.navLink}
-                  color="transparent"
-                >
-                  Places
-                </Button>
-            </ListItem>
-              <ListItem className={classes.listItem}>
-                <Button
-                  href="/events"
-                  className={classes.navLink}
-                  color="transparent"
-                >
-                  Events
-                </Button>
-              </ListItem>
-              <ListItem className={classes.listItem}>
-                <CustomDropdown
-                  center
-                  caret={false}
-                  hoverColor="black"
-                  buttonText={
-                    <img
-                      src={this.props.image ? this.props.image.url.url : "/assets/default_avatar.jpg"}
-                      className={classes.img}
-                      alt="profile"
-                    />
-                  }
-                  buttonProps={{
-                    className:
-                      classes.navLink + " " + classes.imageDropdownButton,
-                    color: "transparent"
-                  }}
-                  dropdownList={[
-                    <a
-                      href={"/users/" + this.props.id}
-                      data-method="get"
-                      className={classes.dropdownLink}
-                    >
-                      Me
-                    </a>,
-                    <a
-                      href={"/users/" + this.props.id + "/edit"}
-                      data-method="get"
-                      className={classes.dropdownLink}
-                    >
-                      Edit Your Profile
-                    </a>,
-                    <a
-                      href="/users/sign_out"
-                      data-method="delete"
-                      className={classes.dropdownLink}
-                    >
-                      Sign out
-                    </a>
-                  ]}
-                />
-              </ListItem>
-            </List>
-          }
+function LoginButtons({classes}){
+  return (
+    <ListItem className={classes.listItem}>
+      <Button
+        href="/users/sign_in"
+        className={classes.navLink}
+        color="transparent"
+      >
+        Sign in
+      </Button>
+      <Button
+        href="/users/sign_up"
+        className={classes.registerNavLink}
+        color="primary"
+        round
+      >
+        Sign up
+      </Button>
+    </ListItem>
+  );
+}
+
+function DropdownBar({image, classes, id}){
+  return (
+    <CustomDropdown
+      center
+      caret={false}
+      hoverColor="black"
+      buttonText={
+        <img
+          src={image ? image.url.url : "/assets/default_avatar.jpg"}
+          className={classes.img}
+          alt="profile"
         />
-      );
-    } else {
-      return (
-        <Header
-          brand="Пешеходка"
-          color="dark"
-          fixed
-          rightLinks={
-            <List className={classes.list}>
-              <ListItem className={classes.listItem}>
-                  <Button
-                    href="/organizations"
-                    className={classes.navLink}
-                    color="transparent"
-                  >
-                    Organizations
-                  </Button>
-              </ListItem>
-              <ListItem className={classes.listItem}>
-                <Button
-                  href="/places"
-                  className={classes.navLink}
-                  color="transparent"
+      }
+      buttonProps={{
+        className:
+          classes.navLink + " " + classes.imageDropdownButton,
+        color: "transparent"
+      }}
+      dropdownList={[
+        <a
+          href={"/users/" + id}
+          data-method="get"
+          className={classes.dropdownLink}
+        >
+          Me
+        </a>,
+        <a
+          href={"/users/" + id + "/edit"}
+          data-method="get"
+          className={classes.dropdownLink}
+        >
+          Edit Your Profile
+        </a>,
+        <a
+          href="/users/sign_out"
+          data-method="delete"
+          className={classes.dropdownLink}
+        >
+          Sign out
+        </a>
+      ]}
+    />
+  );
+}
+
+function UserHasOrganizationsButton({classes}){
+  return (
+    <ListItem className={classes.listItem}>
+      <Button
+        href="/events/new"
+        className={classes.registerNavLink}
+        color="primary"
+        round
+      >
+        CREATE EVENT
+      </Button>
+    </ListItem>
+  );
+}
+
+function UserHasNotOrganizationsButton({classes}){
+  return (
+    <ListItem className={classes.listItem}>
+      <DialogWindow></DialogWindow>
+    </ListItem>
+  );
+}
+
+function Navigation({classes, image, id, current_user, organizations}) {
+    return (
+      <Header
+        brand="Пешеходка"
+        color="dark"
+        fixed
+        rightLinks={
+          <List className={classes.list}>
+            {current_user && <ListItem className={classes.listItem}>
+              {organizations.length
+                ? <UserHasOrganizationsButton classes={classes}/>
+                : <UserHasNotOrganizationsButton classes={classes}/>}
+            </ListItem>}
+            <ListItem className={classes.listItem}>
+              <Button
+                href="/organizations"
+                className={classes.navLink}
+                color="transparent"
                 >
-                  Places
-                </Button>
-              </ListItem>
-              <ListItem className={classes.listItem}>
-                <Button
-                  href="/events"
-                  className={classes.navLink}
-                  color="transparent"
-                >
-                  Events
-                </Button>
-              </ListItem>
-              <ListItem className={classes.listItem}>
-                <Button
-                  href="/users/sign_in"
-                  className={classes.navLink}
-                  color="transparent"
-                >
-                  Sign in
-                </Button>
-              </ListItem>
-              <ListItem className={classes.listItem}>
-                <Button
-                  href="/users/sign_up"
-                  className={classes.registerNavLink}
-                  color="primary"
-                  round
-                >
-                  Sign up
-                </Button>               
-              </ListItem>             
-            </List>
-          }
-        />
-      );
-    }
-  }
+                Organizations
+              </Button>
+            </ListItem>
+            <ListItem className={classes.listItem}>
+              <Button
+                href="/places"
+                className={classes.navLink}
+                color="transparent"
+              >
+                Places
+              </Button>
+            </ListItem>
+            <ListItem className={classes.listItem}>
+              <Button
+                href="/events"
+                className={classes.navLink}
+                color="transparent"
+              >
+                Events
+              </Button>
+            </ListItem>
+            <ListItem className={classes.listItem}>
+              {id ?
+              <DropdownBar classes={classes} image={image} id={id}/> :
+
+              <LoginButtons classes={classes} image={image} id={id}/>
+              }
+            </ListItem>
+          </List>
+        }
+      />
+    );
 }
 
 Navigation.propTypes = {
