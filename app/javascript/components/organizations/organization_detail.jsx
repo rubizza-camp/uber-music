de7afe5f47afbb26811b4cba0eb67d5a0157b5e8
MariaPaharelava <paharelava.maria@gmail.com>
2019-08-26 17:53:49 +0300

    Fix errors

--- app/javascript/components/organizations/organization_detail.jsx
+++ app/javascript/components/organizations/organization_detail.jsx
@@ -15,42 +15,56 @@ class OrganizationDetail extends React.Component {
     return (
       <div>
         <Grid container direction="column" justify="center" alignItems="center">
-          <Grid item>
-            <OrganizationTitle current_user={current_user} users={organization.users}
-                               organization_name={organization.name} organization_id={organization.id}/>
+          <Grid container direction="row">
+            <Grid item xs={12}>
+              <OrganizationTitle current_user={current_user} users={organization.users}
+                                 organization_name={organization.name} organization_id={organization.id}/>
+            </Grid>
           </Grid>
-          <Grid item>
-            <TypoGraphy>
-              <Box component='text' fontSize="21px" fontWeight="300">
-                {organization.description}
-              </Box>
-            </TypoGraphy>
+          
+          <Grid container direction="row" spacing={3}>
+            
+            <Grid item>
+              <TypoGraphy>
+                <Box component='span' fontSize="21px" fontWeight="300">
+                  {organization.description}
+                </Box>
+              </TypoGraphy>
+            </Grid>
+            <br/>
+            
+            
+            <Grid item xs={11} md={12}>
+              <ImageCarousel images={organization.images.map((image) => image.url.url)} slidesToShow={1}
+                             slidesToScroll={1} width={'100%'} height={'50%'}/>
+            </Grid>
+            
+            
+            <Grid item>
+              <TypoGraphy component="h3">
+                <Box fontSize="21px" fontWeight='300'>
+                  For each event we carry out a program! Every holiday is important to us.
+                  Nisi nisi magna deserunt tempor velit nulla ad.
+                </Box>
+              </TypoGraphy>
+            </Grid>
+          
           </Grid>
+          
           <br/>
-          <Grid item xs={11} md={12}>
-            <ImageCarousel images={organization.images.map((image) => image.url.url)} slidesToShow={1}
-                           slidesToScroll={1} width={'100%'} height={'50%'}/>
-          </Grid>
-          <Grid item>
-            <TypoGraphy component="h3">
-              <Box fontSize="21px" fontWeight='300'>
-                For each event we carry out a program! Every holiday is important to us.
-                Nisi nisi magna deserunt tempor velit nulla ad.
-              </Box>
-            </TypoGraphy>
-          </Grid>
           <br/>
-          <br/>
-          <Grid item>
+          
+          <Grid container direction="row">
             <Grid container direction="row" justify="center" alignItems="stretch">
               {organization.approved_events.map((event, i) =>
                 <Grid item xs={12} sm={6} md={4} key={i}>
                   <EventCard key={i} name={event.name} addres={'##'} start_time={event.start_time}
-                             end_time={event.end_time} src={event.images[0].url.url} link={`/events/${event.id}`}></EventCard>
+                             end_time={event.end_time} src={event.images[0].url.url} link={`/events/${event.id}`}/>
                 </Grid>
               )}
             </Grid>
           </Grid>
+          
           <Grid item>
             <TypoGraphy component="h3">
               <Box fontSize="3.0em" fontWeight='100'>
@@ -62,14 +76,14 @@ class OrganizationDetail extends React.Component {
             {organization.users.map((user, i) =>
               <Grid item key={i}>
                 <a href={`/users/${user.id}`}>
-                  <MyImage key={i} src={user.image.url.url} width={'300px'} height={'150px'}></MyImage>
+                  <MyImage key={i} src={user.image.url.url} width={'300px'} height={'150px'}/>
                 </a>
               </Grid>
             )}
           </Grid>
         </Grid>
       </div>
-      
+    
     );
   }
 }
