4a98bfa98a127852b0977b6cf1382e5d8e966da5
MariaPaharelava <paharelava.maria@gmail.com>
2019-08-29 20:38:20 +0300

    BUG26 fix add photo in user

--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -2,7 +2,7 @@ class UserService
   def self.set_image(user, image)
     user&.image&.destroy
     Image.create(imageable_id: user.id,
-                 imageable_type: user.type,
+                 imageable_type: 'User',
                  url: image)
   end
 end
