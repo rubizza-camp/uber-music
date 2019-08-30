f96b12a6ab775fe271c2972bc90187ce23ee29e8
YanFym <dfefeww@gmail.com>
2019-08-30 18:21:14 +0300

    function optimization

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -18,24 +18,16 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   def resize_and_crop(size)
     manipulate! do |image|
-      if image[:width] < image[:height]
-        height_shave(image)
-      elsif image[:width] > image[:height]
-        width_shave(image)
-      end
+      shave(image)
       image.resize("#{size}x#{size}")
-      image
     end
   end
 
   private
 
-  def height_shave(image)
+  def shave(image)
     remove = ((image[:height] - image[:width]) / 2).round
     image.shave("0x#{remove}")
-  end
-
-  def width_shave(image)
     remove = ((image[:width] - image[:height]) / 2).round
     image.shave("#{remove}x0")
   end
