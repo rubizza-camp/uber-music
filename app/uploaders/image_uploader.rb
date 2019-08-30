9cf012aa520ac5ba95ed7b4d9dfcf7bfdf689c0f
YanFym <dfefeww@gmail.com>
2019-08-30 17:12:21 +0300

    New style

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -19,14 +19,24 @@ class ImageUploader < CarrierWave::Uploader::Base
   def resize_and_crop(size)
     manipulate! do |image|
       if image[:width] < image[:height]
-        remove = ((image[:height] - image[:width]) / 2).round
-        image.shave("0x#{remove}")
+        height_shave(image)
       elsif image[:width] > image[:height]
-        remove = ((image[:width] - image[:height]) / 2).round
-        image.shave("#{remove}x0")
+        width_shave(image)
       end
       image.resize("#{size}x#{size}")
       image
     end
+  end
+
+  private
+
+  def height_shave(image)
+    remove = ((image[:height] - image[:width]) / 2).round
+    image.shave("0x#{remove}")
+  end
+
+  def width_shave(image)
+    remove = ((image[:width] - image[:height]) / 2).round
+    image.shave("#{remove}x0")
   end
 end
